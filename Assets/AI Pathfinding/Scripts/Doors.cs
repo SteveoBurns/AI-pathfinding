714aae50a214b497b8a97b5554d2efb8c90a21be
SteveoBurns <steveo.burns.777@gmail.com>
2021-05-04 20:11:22 +1000

    trying to make doors work!

--- Assets/AI Pathfinding/Scripts/Doors.cs
+++ Assets/AI Pathfinding/Scripts/Doors.cs
@@ -27,9 +27,11 @@ public class Doors : MonoBehaviour
         {
             foreach (GameObject door in redDoors)
             {
+                Vector3 doorPos2;
+                Vector3 doorPos = new Vector3(door.gameObject.transform.position.x, door.gameObject.transform.position.y, door.gameObject.transform.position.z);
                 // not sure why but the door is moving below the ground plane & below the doors container.
                 // maybe need to set the position of each door in the loop first? and then move it from that position?
-                door.gameObject.transform.position = new Vector3(door.gameObject.transform.position.x, Mathf.Lerp(door.gameObject.transform.position.y, (door.gameObject.transform.position.y - 2.1f), 0.005f), door.gameObject.transform.position.z);
+                doorPos2 = new Vector3(doorPos.x, Mathf.Lerp(doorPos.y, (doorPos.y - 2.1f), 0.005f), doorPos.z);
             }
         }
         /*else if (!redDoorsOpen)
