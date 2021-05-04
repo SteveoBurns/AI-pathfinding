32ddde8d84f53bed843cdf47c0b47842d910dde3
SteveoBurns <steveo.burns.777@gmail.com>
2021-05-04 17:51:29 +1000

    update door script

--- Assets/AI Pathfinding/Scripts/Doors.cs
+++ Assets/AI Pathfinding/Scripts/Doors.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    public bool isOpen = false;
+    [Header("Red Doors")]
+    [SerializeField] private List<GameObject> redDoors; 
+
+    public bool redDoorsOpen = false;
 
     private float closePos;
     private float openPos;
@@ -12,7 +15,7 @@ public class Doors : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isOpen = false;
+        redDoorsOpen = false;
         closePos = gameObject.transform.position.y;
         openPos = transform.position.y - 2.1f;
     }
@@ -20,11 +23,35 @@ public class Doors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        OpenDoors();
+        if (redDoorsOpen) // need to put another condition like a counter here to stop it happening every frame.
+        {
+            foreach (GameObject door in redDoors)
+            {
+                // not sure why but the door is moving below the ground plane & below the doors container.
+                // maybe need to set the position of each door in the loop first? and then move it from that position?
+                door.gameObject.transform.position = new Vector3(door.gameObject.transform.position.x, Mathf.Lerp(door.gameObject.transform.position.y, (door.gameObject.transform.position.y - 2.1f), 0.005f), door.gameObject.transform.position.z);
+            }
+        }
+        /*else if (!redDoorsOpen)
+        {
+            foreach (GameObject door in redDoors)
+            {
+                door.gameObject.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, (transform.position.y + 2.1f), 0.005f), transform.position.z);
+            }
+        }*/
     }
 
     public void OpenDoors()
     {
        transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, openPos, 0.005f), transform.position.z);
     }
+    public void CloseDoors()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, closePos, 0.005f), transform.position.z);
+    }
+
+    
+
+
+    
 }
