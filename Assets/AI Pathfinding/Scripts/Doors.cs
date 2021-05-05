4f91dbb421d724bc2f37d29be7fbcd0d4787684d
Steveo Burns <steveo.burns.777@gmail.com>
2021-05-05 17:10:42 +1000

    Doors Update
    Doors work using a bool
    Doors have nav mesh obstacle and carve.

--- Assets/AI Pathfinding/Scripts/Doors.cs
+++ Assets/AI Pathfinding/Scripts/Doors.cs
@@ -1,46 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Doors : MonoBehaviour
 {
     [Header("Red Doors")]
-    [SerializeField] private List<GameObject> redDoors; 
+    [SerializeField] private List<GameObject> redDoors;
+
+    [Header("Green Doors")]
+    [SerializeField] private List<GameObject> greenDoors;
+    
+    
 
     public bool redDoorsOpen = false;
 
-    private float closePos;
-    private float openPos;
+    private float closePos = 0;
+    private float openPos = -2.1f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         redDoorsOpen = false;
-        closePos = gameObject.transform.position.y;
-        openPos = transform.position.y - 2.1f;
+        //closePos = gameObject.transform.position.y;
+        //openPos = transform.position.y - 2.1f;
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         if (redDoorsOpen) // need to put another condition like a counter here to stop it happening every frame.
         {
+            // Open all red doors
             foreach (GameObject door in redDoors)
             {
                 Vector3 doorPos2;
                 Vector3 doorPos = new Vector3(door.gameObject.transform.position.x, door.gameObject.transform.position.y, door.gameObject.transform.position.z);
-                // not sure why but the door is moving below the ground plane & below the doors container.
-                // maybe need to set the position of each door in the loop first? and then move it from that position?
-                doorPos2 = new Vector3(doorPos.x, Mathf.Lerp(doorPos.y, (doorPos.y - 2.1f), 0.005f), doorPos.z);
+                
+                
+                doorPos2 = new Vector3(doorPos.x, 
+                    Mathf.Lerp(doorPos.y, openPos, 0.005f),
+                    doorPos.z);
+
+
+                door.transform.position = doorPos2;
+            }
+
+            //Close all green doors when red doors are open
+            foreach (GameObject door in greenDoors)
+            {
+                Vector3 doorPos2;
+                Vector3 doorPos = new Vector3(door.gameObject.transform.position.x, door.gameObject.transform.position.y, door.gameObject.transform.position.z);
+
+
+                doorPos2 = new Vector3(doorPos.x,
+                    Mathf.Lerp(doorPos.y, closePos, 0.005f),
+                    doorPos.z);
+
+
+                door.transform.position = doorPos2;
             }
         }
-        /*else if (!redDoorsOpen)
+        else if (!redDoorsOpen)
         {
+            // Close all red doors
             foreach (GameObject door in redDoors)
             {
-                door.gameObject.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, (transform.position.y + 2.1f), 0.005f), transform.position.z);
+                Vector3 doorPos2;
+                Vector3 doorPos = new Vector3(door.gameObject.transform.position.x, door.gameObject.transform.position.y, door.gameObject.transform.position.z);
+
+
+                doorPos2 = new Vector3(doorPos.x,
+                    Mathf.Lerp(doorPos.y, closePos, 0.005f),
+                    doorPos.z);
+
+
+                door.transform.position = doorPos2;
             }
-        }*/
+
+            // Open all green doors
+            foreach (GameObject door in greenDoors)
+            {
+                Vector3 doorPos2;
+                Vector3 doorPos = new Vector3(door.gameObject.transform.position.x, door.gameObject.transform.position.y, door.gameObject.transform.position.z);
+
+
+                doorPos2 = new Vector3(doorPos.x,
+                    Mathf.Lerp(doorPos.y, openPos, 0.005f),
+                    doorPos.z);
+
+
+                door.transform.position = doorPos2;
+            }
+        }
     }
 
     public void OpenDoors()
